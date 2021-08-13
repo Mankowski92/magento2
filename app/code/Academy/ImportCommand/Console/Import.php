0632301633dd189006e343823c1dc37b141b810d
Marcin Mańkowski <marcin.mankowski@solteq.com>
2021-08-13 08:43:51 +0200

    test

--- app/code/Academy/ImportCommand/Console/Import.php
+++ app/code/Academy/ImportCommand/Console/Import.php
@@ -2,9 +2,13 @@
 
 namespace Academy\ImportCommand\Console;
 
-use Magento\Framework\Exception\FileSystemException;
+use Magento\Catalog\Model\ProductFactory;
+use Magento\Framework\App\Area;
+use Magento\Framework\App\State;
 use Magento\Framework\Filesystem\Driver\File;
 use Magento\Framework\Module\Dir\Reader;
+use Magento\Framework\Serialize\Serializer\Json;
+use Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -12,10 +16,106 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Import extends Command
 {
-
-    public const FILE_NAME_ARGUMENT = 'file';
-    public const IMPORT_DIR = '/import/';
     const FILE = 'file';
+    const SERIALIZED = 'serialized';
+    const VALUE = 'value';
+
+
+    protected $reader;
+    protected $file;
+    protected $json;
+    protected $fileName = null;
+    protected $serialized = null;
+    protected $value = null;
+    protected $productFactory;
+    protected $resourceModel;
+
+    protected $state;
+    protected $storeManager;
+
+    public function __construct(
+        Json                                         $json,
+        Reader                                       $reader,
+        File                                         $file,
+        ProductFactory                               $productFactory,
+        State                                        $state,
+        \Magento\Store\Model\StoreManagerInterface   $storeManager,
+        \Magento\Catalog\Model\ResourceModel\Product $resourceModel
+
+    )
+    {
+        parent::__construct();
+        $this->reader = $reader;
+        $this->file = $file;
+        $this->json = $json;
+        $this->state = $state;
+        $this->storeManager = $storeManager;
+        $this->productFactory = $productFactory;
+        $this->resourceModel = $resourceModel;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $this->state->setAreaCode(Area::AREA_ADMINHTML);
+        $this->storeManager->setCurrentStore($this->storeManager->getDefaultStoreView()->getWebsiteId());
+
+        $product = $this->productFactory->create();
+        $test = $this->resourceModel->save($product);
+
+        try {
+            $test = 'DUPA DUPA';
+            $fileName = $input->getOption(self::FILE);
+            $serialized = $input->getOption(self::SERIALIZED);
+            $value = $input->getOption(self::VALUE);
+            echo $test;
+
+
+            $moduleEtcPath = $this->reader->getModuleDir(
+                \Magento\Framework\Module\Dir::MODULE_ETC_DIR,
+                'Academy_ImportCommand'
+            );
+
+            $fullFileName = $moduleEtcPath . '/' . $fileName;
+            $fileContent = $this->file->fileGetContents($fullFileName);
+            $unserializedData = $this->json->unserialize($fileContent);
+
+            $output->writeln("\n<info>You are looking for $fileName \n</info>");
+            $output->writeln("Full path to file: $fullFileName\n");
+            $output->writeln("$test");
+
+            if ($this->file->isExists($fullFileName)) {
+                $output->writeln("<comment>FILE EXIST\n</comment>");
+                $output->writeln("<info>File content: \n </info>" . $fileContent);
+            } else {
+                $output->writeln('<error>FILE DOES NOT EXIST</error>');
+            }
+
+            if ($serialized) {
+                $output->writeln("<info>Unserialized data: </info>");
+
+                var_dump($unserializedData) . "\n";
+            } else {
+                $output->writeln("<info>Put flag '--serialized true' to see unserialized content\n </info> ");
+            }
+
+            $keys_array = array_keys($unserializedData['product']);
+            $isExisting = (in_array("$value", $keys_array));
+
+            if ($value && $isExisting) {
+                $unserializedData = $this->json->unserialize($fileContent);
+                $output->writeln("\nvalue of $value is " . $unserializedData['product']["$value"]);
+            } else {
+                $output->writeln("<error>\nThere is no key $value. Please try one of:</error>\n");
+                foreach ($keys_array as $item) {
+                    echo "$item \n";
+                }
+                echo "\n";
+            }
+        } catch (FileSystemException $e) {
+            echo $e;
+        }
+    }
 
     protected function configure()
     {
@@ -27,49 +127,18 @@ class Import extends Command
             InputOption::VALUE_REQUIRED,
             'File'
         );
-
+        $this->addOption(
+            self::SERIALIZED,
+            null,
+            InputOption::VALUE_NONE,
+            'Serialized'
+        );
+        $this->addOption(
+            self::VALUE,
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Value'
+        );
         parent::configure();
-    }
-
-    protected $reader;
-    protected $file;
-    protected $fileName = null;
-
-    public function __construct(
-        reader $reader,
-        File   $file
-    )
-    {
-        parent::__construct();
-        $this->reader = $reader;
-        $this->file = $file;
-    }
-
-
-    protected
-    function execute(InputInterface $input, OutputInterface $output)
-    {
-
-//        DirectoryList::VAR_DIR
-
-        try {
-            $fileName = $input->getOption(self::FILE);
-
-            $moduleEtcPath = $this->reader->getModuleDir(
-                \Magento\Framework\Module\Dir::MODULE_ETC_DIR,
-                'Academy_ImportCommand'
-            );
-
-            $fullFileName = $moduleEtcPath . '/' . $fileName;
-            $output->writeln('<info>You are looking for ' . $fileName . '</info>');
-            $output->writeln('Full path to file: ' . $moduleEtcPath . '/' . $fileName);
-            if ($this->file->isExists($fullFileName)) {
-                echo "FILE EXIST\n";
-            } else {
-                echo "FILE DOES NOT EXIST\n";
-            }
-        } catch (FileSystemException $e) {
-            echo $e;
-        }
     }
 }
